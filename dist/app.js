2504d923f2b0d26e7151d1680f874b00e307118e
stevewang <privacyalwaysfirst@proton.me>
2022-11-11 02:21:41 -0800

    fix

--- dist/app.js
+++ dist/app.js
@@ -1,3 +1,18 @@
 "use strict";
-const num = 1;
-console.log(num);
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+app.post("/", (req, res) => {
+    console.log(req.body);
+    res.send();
+});
+app.listen(3000, () => {
+    console.log("Server started on port 3000");
+});
--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express from "express";
 
 const app: express.Application = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
